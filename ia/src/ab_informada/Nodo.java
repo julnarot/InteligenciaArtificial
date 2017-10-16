bbf47101534fd5cff91c2e2f8ec8a3cd66713980
Raul Jonatan <rauljhonatan.tola@gmail.com>
2017-10-15 19:07:42 -0500

    nodo is added to first package

---
+++ ia/src/ab_informada/Nodo.java
@@ -0,0 +1,41 @@
+package ab_informada;
+
+import java.util.List;
+
+public class Nodo {
+    int[] datos;
+    Nodo padre;
+    List<Nodo> hijos;
+    public int[] getDatos() {
+        return datos;
+    }
+
+    public void setDatos(int[] datos) {
+        this.datos = datos;
+    }
+
+    public Nodo getPadre() {
+        return padre;
+    }
+
+    public void setPadre(Nodo padre) {
+        this.padre = padre;
+    }
+
+    public List<Nodo> getHijos() {
+        return hijos;
+    }
+
+    public void setHijos(List<Nodo> hijos) {
+        this.hijos = hijos;
+        if(hijos != null){
+            for (Nodo n: hijos
+                 ) {
+                n.padre = this;
+            }
+        }
+    }
+
+
+
+}
